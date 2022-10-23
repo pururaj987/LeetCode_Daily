63e4fac195d839a318d175a576e1e9b119ae6e2f
Pururaj Singh Rajawat <pururajsrajawwat@gmail.com>
2022-10-23 21:46:36 +0530

    Set Mismatch

--- Oct_23.cpp
+++ Oct_23.cpp
@@ -1,21 +1,21 @@
-https://leetcode.com/problems/set-mismatch/description/
+// https://leetcode.com/problems/set-mismatch/description/
 
-You have a set of integers s, which originally contains all the numbers from 1 to n. 
-Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+// You have a set of integers s, which originally contains all the numbers from 1 to n. 
+// Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
 
-You are given an integer array nums representing the data status of this set after the error.
+// You are given an integer array nums representing the data status of this set after the error.
 
-Find the number that occurs twice and the number that is missing and return them in the form of an array.
+// Find the number that occurs twice and the number that is missing and return them in the form of an array.
 
-Input: nums = [1,2,2,4]
-Output: [2,3]
+// Input: nums = [1,2,2,4]
+// Output: [2,3]
 
-Input: nums = [1,1]
-Output: [1,2]
+// Input: nums = [1,1]
+// Output: [1,2]
 
-Approach - 1 -- Using Set
-TC - O(N)
-SC - O(N)
+// Approach - 1 -- Using Set
+// TC - O(N)
+// SC - O(N)
 
 class Solution {
 public:
@@ -49,9 +49,9 @@ public:
     }
 };
 
-Approach - 2 -- Without set
-TC - O(NLogN)
-SC - O(1)
+// Approach - 2 -- Without set
+// TC - O(NLogN)
+// SC - O(1)
 
 class Solution {
 public:
