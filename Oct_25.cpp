e15192b307100147571abb5b055d5b2d818cf49e
Pururaj Singh Rajawat <pururajsrajawwat@gmail.com>
2022-10-25 10:58:44 +0530

    Check if two string arrays are equivalent

---
+++ Oct_25.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+// A string is represented by an array if the array elements concatenated in order forms the string.
+
+// Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+// Output: true
+// Explanation:
+// word1 represents string "ab" + "c" -> "abc"
+// word2 represents string "a" + "bc" -> "abc"
+// The strings are the same, so return true.
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1 = "" , s2 = "";
+        for(auto str: word1)
+            s1 += str;
+        for(auto str: word2)
+            s2 += str;
+        return s1 == s2;
+    }
+};
