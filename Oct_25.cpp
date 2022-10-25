f44df756e7f578031c76d134de2f315e511334d3
Pururaj Singh Rajawat <pururajsrajawwat@gmail.com>
2022-10-25 12:21:24 +0530

    Check if two string arrays are equivalent

--- Oct_25.cpp
+++ Oct_25.cpp
@@ -22,3 +22,40 @@ public:
         return s1 == s2;
     }
 };
+
+
+Approach 2 - Keep 4 pointers
+TC - O(N)
+SC - O(1)
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        // keep track of current chars in string
+        int i = 0 , j = 0;
+        // keep track of current string
+        int st1 = 0 , st2 = 0;
+        while(st1 < word1.size() && st2 < word2.size()) {
+            int curr_len_first = word1[st1].size();
+            int curr_len_second = word2[st2].size();
+            while(i < curr_len_first && j < curr_len_second) {
+                // if currrent characters don't match
+                if(word1[st1][i] != word2[st2][j]) {
+                    return false;
+                }
+                i++ , j++;
+            }
+            // if first string is smaller , move to next string in first array and reset i to 0
+            if(i == curr_len_first) {
+                i = 0;
+                st1 ++;
+            }
+            // do same thing with j as well
+            if(j == curr_len_second) {
+                j = 0;
+                st2 ++;
+            }
+        }
+        return (st1 == word1.size() && st2 == word2.size());
+    }
+};
